29067a674b222b2293999463e7df6814b36af837
Tige Phillips <tigelane@users.noreply.github.com>
2015-07-29 16:39:57 -0700

    Some error checking and screen output option

--- aci-to-twitter.py
+++ aci-to-twitter.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 """
-Application using event subscription to forward events to Twitter
+Application using event subscription to forward events to a Twitter
 user ID.  Anyone can then subscribe and get immediate information
-is the events happen in ACI.
+regarding events happen in an ACI environment.
 """
 import sys
 import acitoolkit.acitoolkit as aci
 import tweepy
 
-#  These should be changed to your application information, but it's ok to try them a few times.
+#  Twitter integration
 ckey = ''
 csecret = ''
 atoken = ''
@@ -17,14 +17,20 @@ asecret = ''
 
 auth = tweepy.OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
-api = tweepy.API(auth)
+twitter = tweepy.API(auth)
+
+# Valid options are 'screen' and 'twitter'
+output = 'screen'
 
 def display(message):
-    print message
-    try:
-        api.update_status(message)
-    except:
-        print "There was a problem posting to twitter.\n"
+
+    if output == 'screen':
+        print message
+    if output == 'twitter':
+        try:
+            twitter.update_status(message)
+        except:
+            print "There was a problem posting to twitter.\n"
 
 def main():
     """
@@ -43,9 +49,9 @@ def main():
     if not resp.ok:
         print('%% Could not login to APIC')
         sys.exit(0)
-        
     
 
+    # Register all of the events we want to watch for.  Comment them out if you don't want to see them
     aci.Tenant.subscribe(session)
     aci.AppProfile.subscribe(session)
 
@@ -54,15 +60,18 @@ def main():
             tenant = aci.Tenant.get_event(session)
             if tenant.is_deleted():
                 message = 'Tenant:' + tenant.name + ' has been deleted.'
+                display(message)
             else:
                 message = 'Tenant:' + tenant.name + ' has been created or modified.'
-            display(message)
+                display(message)
+            
 
         if aci.AppProfile.has_events(session):
             app_profile = aci.AppProfile.get_event(session)
             tenant = app_profile.get_parent()
-            message = 'Tenant:' + tenant.name + ' Application:' + app_profile.name + ' has changed.'
+            message = 'Application:' + app_profile.name + ' in tenant:' + tenant.name +  ' has changed.'
             display(message)
+            
 
 if __name__ == '__main__':
     try:
