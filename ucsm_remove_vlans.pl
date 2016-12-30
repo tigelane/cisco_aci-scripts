71677e2b8ea7fa9f8d5cbbfd7c56c4fcc7796f36
tigelane <tige@tigelane.com>
2016-12-30 09:48:26 -0800

    Updates for what we did on the last lab day.

---
+++ ucsm_remove_vlans.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from ucsmsdk.ucshandle import UcsHandle
+
+# Create a connection handle
+handle = UcsHandle("172.31.0.5", "admin", "cisco!098")
+
+# Put in the VLANs you want to remove first one, and then the last one.
+vlan_start = 2048
+vlan_end = 2052
+vlan_name_prefix = "test_vlan_"
+
+# Login to the server
+handle.login()
+
+
+for a in range(vlan_start, vlan_end + 1):
+    mydn = vlan_name_prefix + str(a)
+    print "Removing " + (mydn)
+    myfulldn = "fabric/lan/net-" + mydn
+
+    # Query for an existing Mo
+    sp = handle.query_dn(myfulldn)
+
+    # Remove the object
+    handle.remove_mo(sp)
+    # and commit the changes (actually happens now)
+    handle.commit()
+
+
+# Logout from the server
+handle.logout()
